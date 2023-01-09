c67c6df7f1742781911da0f3b0b535bc0e88a9fd
vivekkumar854301 <vivekkumar2020@vitbhopal.ac.in>
2023-01-10 00:38:25 +0530

    Create Apples and Oranges.cpp

---
+++ Apples and Oranges.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x,a,b;
+	cin>>x>>a>>b;
+	if(x>a+b){
+	    cout<<"YES"<<endl;
+	}
+	else{
+	    cout<<"NO"<<endl;
+	}
+	return 0;
+}
